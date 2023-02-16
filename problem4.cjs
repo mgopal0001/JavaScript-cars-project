93736f1b3163684d57d0ef3f84fc4b281b9dd351
Madan Gopal <madan.23.2@mountblue.tech>
2023-02-16 16:31:01 +0530

    Bug Fixes in problem 4

--- problem4.cjs
+++ problem4.cjs
@@ -1,8 +1,21 @@
 function problem4(carsInventory){
+
+    if(problem4.arguments.length < 1){
+        return [];
+    }
+    if(carsInventory.length == 0){
+        return [];
+    }
+    if(!Array.isArray(carsInventory)){
+        return [];
+    }
+
     let carYear = [];
-    for(let rowIndex=0;rowIndex<carsInventory.length;rowIndex++){
+    
+    for(let rowIndex = 0; rowIndex < carsInventory.length; rowIndex++){
         carYear[rowIndex] = carsInventory[rowIndex].car_year;
     }
+
     return carYear;
 }
 module.exports = problem4;
