96e806ae54473abab9a04c181720f0cd3a63d0b0
Madan Gopal <madan.23.2@mountblue.tech>
2023-02-16 16:49:53 +0530

    Bug Fixes in problem 5 as well as in testProblem 5

--- problem5.cjs
+++ problem5.cjs
@@ -1,16 +1,34 @@
-function problem5(allCarYears){
+function problem5(allCarYears , olderYear){
+
+    if(problem5.arguments.length < 1){
+        return [1];
+    }
+    if(allCarYears.length == 0){
+        return [2];
+    }
+    if(!Array.isArray(allCarYears)){
+        return [3];
+    }
+    if(typeof olderYear == 'undefined'){
+        return [4];
+    }
+    if(typeof(olderYear) != 'number'){
+        return [5];
+    }
+
     allCarYears.sort();
+
     let carYearsAfterCondition = [];
 
     for(let rowIndex = 0; rowIndex < allCarYears.length; rowIndex++){
 
-        if(allCarYears[rowIndex] >= 2000){
+        if(allCarYears[rowIndex] >= olderYear){
             break;
         }
 
         carYearsAfterCondition[rowIndex] = allCarYears[rowIndex];
     }
-    console.log('Total Cars older than 2000: ' + carYearsAfterCondition.length);
+    console.log('Total Cars older than '+olderYear+': ' + carYearsAfterCondition.length);
     return carYearsAfterCondition;
 }
 module.exports = problem5;
--- test/testProblem5.cjs
+++ test/testProblem5.cjs
@@ -3,9 +3,7 @@ const problem4 = require('../problem4.cjs');
 const problem5 = require('../problem5.cjs');
 
 const allCarYears = problem4(inventory);
-const result = problem5(allCarYears);
+const result = problem5(allCarYears, 2000);
+console.log(result);
 
-for(let rowIndex = 0; rowIndex < result.length; rowIndex++){
-    console.log(result[rowIndex]);
-}
 
