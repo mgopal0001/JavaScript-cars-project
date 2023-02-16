b8a41f163d94c3eda3a69d764ddebbc7f3cc873f
Madan Gopal <madan.23.2@mountblue.tech>
2023-02-16 16:20:03 +0530

    Bug Fixes in problem 3

--- problem3.cjs
+++ problem3.cjs
@@ -1,8 +1,20 @@
 function problem3(carsInventory){
+
+    if(problem3.arguments.length < 1){
+        return [];
+    }
+    if(carsInventory.length == 0){
+        return [];
+    }
+    if(!Array.isArray(carsInventory)){
+        return [];
+    }
+
     let carModel = [];
     for(let rowIndex = 0; rowIndex<carsInventory.length; rowIndex++){
         carModel[rowIndex] = carsInventory[rowIndex].car_model;
     }
+    
     return carModel.sort();
 }
 module.exports = problem3;
